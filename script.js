29e16c8c21cb36fb8e64bdc323e4b536570a998f
James Wood <james.s.wood@icloud.com>
2023-06-03 05:47:47 -0400

    Expanded function of the script.js file.

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
     changeBtn.addEventListener('click', function() {
         messageElement.textContent = 'Message changed!';
     });
+    var counterElement = document.getElementById('counter');
+    var incrementBtn = document.getElementById('incrementBtn');
+    var resetBtn = document.getElementById('resetBtn');
+    var counterValue = 0;
+
+    incrementBtn.addEventListener('click', function() {
+        counterValue++;
+        counterElement.textContent = counterValue;
+    });
+
+    resetBtn.addEventListener('click', function() {
+        counterValue = 0;
+        counterElement.textContent = counterValue;
+    });
+
+
 });
 
